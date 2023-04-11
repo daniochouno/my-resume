3cb63f8108063a0f52994f91254a8920a32726ce
Daniel Martínez <developer@danielmartinez.info>
2023-04-11 18:40:37 +0200

    Initial commit.

---
+++ my-resume/my_resumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_resumeApp.swift
+//  my-resume
+//
+//  Created by Daniel Martínez Muñoz on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct my_resumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ my-resumeUITests/my_resumeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  my_resumeUITestsLaunchTests.swift
+//  my-resumeUITests
+//
+//  Created by Daniel Martínez Muñoz on 11/4/23.
+//
+
+import XCTest
+
+final class my_resumeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
