7952b6f4bbe3e38f90fcc01d11c22caea7193990
Daniel Martínez <developer@danielmartinez.info>
2023-04-27 15:10:14 +0200

    Added SessionFirestoreModel.

---
+++ myResume/data/models/firestore/SessionFirestoreModel.swift
@@ -0,0 +1,14 @@
+//
+//  SessionFirestoreModel.swift
+//  myResume
+//
+//  Created by Daniel Martínez Muñoz on 26/4/23.
+//
+
+import Foundation
+
+struct SessionFirestoreModel: Decodable {
+    let idToken: String
+    let refreshToken: String
+    let expiresIn: Int
+}
