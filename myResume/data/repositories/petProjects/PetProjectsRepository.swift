386b71cbe550f2ea3c00e67b268172461433f129
Daniel Martínez <developer@danielmartinez.info>
2023-05-04 21:25:57 +0200

    Pass tests.

--- myResume/data/repositories/petProjects/PetProjectsRepository.swift
+++ myResume/data/repositories/petProjects/PetProjectsRepository.swift
@@ -21,6 +21,47 @@ class PetProjectsRepositoryImpl: PetProjectsRepository {
     }
     
     func fetch() async -> Result<[PetProjectFirestoreModel], Error> {
-        return .failure(APIResponseError.configuration)
+        await authenticateIfNeeded()
+        
+        let result = await dataSource.fetchPetProjects()
+        switch result {
+        case .success(let documents):
+            let petProjects = documents.documents
+            return .success(petProjects)
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+    
+    private func authenticateIfNeeded() async {
+        guard let _ = userDefaults.string(forKey: "access_token"),
+                let _ = userDefaults.string(forKey: "refresh_token") else {
+            await authenticate()
+            return
+        }
+        
+        let expiresAt = userDefaults.double(forKey: "expires_at")
+        let currentTimestamp = Date().timeIntervalSince1970
+        guard expiresAt >= currentTimestamp else {
+            await authenticate()
+            return
+        }
+    }
+    
+    private func authenticate() async {
+        let result = await dataSource.signIn()
+        switch result {
+        case .success(let session):
+            let idToken = session.idToken
+            let refreshToken = session.refreshToken
+            let expiresIn = session.expiresIn
+            let expiresAt = Date().addingTimeInterval(Double(expiresIn) ?? 0)
+            
+            self.userDefaults.set(idToken, forKey: "access_token")
+            self.userDefaults.set(refreshToken, forKey: "refresh_token")
+            self.userDefaults.set(expiresAt.timeIntervalSince1970, forKey: "expires_at")
+        case .failure:
+            break
+        }
     }
 }
