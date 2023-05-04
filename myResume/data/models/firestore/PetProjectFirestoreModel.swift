aee04d77e00c2efbda2ec7aba4ae7d7f11f2a061
Daniel Martínez <developer@danielmartinez.info>
2023-05-04 21:10:44 +0200

    Added PetProject Firestore model.

---
+++ myResume/data/models/firestore/PetProjectFirestoreModel.swift
@@ -0,0 +1,31 @@
+//
+//  PetProjectFirestoreModel.swift
+//  myResume
+//
+//  Created by Daniel Martínez Muñoz on 26/4/23.
+//
+
+import Foundation
+
+struct PetProjectDocumentsFirestoreModel: Decodable {
+    let documents: [PetProjectFirestoreModel]
+}
+
+struct PetProjectFirestoreModel: Decodable {
+    let name: String
+    let fields: PetProjectFieldFirestoreModel
+}
+
+struct PetProjectFieldFirestoreModel: Decodable {
+    let titleKey: PetProjectFieldStringValueFirestoreModel
+    let subtitleKey: PetProjectFieldStringValueFirestoreModel
+    let iconUrl: PetProjectFieldStringValueFirestoreModel
+    let linkAppStore: PetProjectFieldStringValueFirestoreModel?
+    let linkPlayStore: PetProjectFieldStringValueFirestoreModel?
+    let linkWeb: PetProjectFieldStringValueFirestoreModel?
+    let downloads: PetProjectFieldStringValueFirestoreModel?
+}
+
+struct PetProjectFieldStringValueFirestoreModel: Decodable {
+    let stringValue: String
+}
