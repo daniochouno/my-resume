febb42faaa79905276c497c55c5a54fababccde5
Daniel Martínez <developer@danielmartinez.info>
2023-05-04 21:35:08 +0200

    Added tests for fetching Pet Projects in use case.

---
+++ myResumeTests/domain/useCases/petProjects/FetchPetProjectsUseCaseTests.swift
@@ -0,0 +1,44 @@
+//
+//  FetchPetProjectsUseCaseTests.swift
+//  myResumeTests
+//
+//  Created by Daniel Martínez Muñoz on 4/5/23.
+//
+
+import XCTest
+@testable import myResume
+
+final class FetchPetProjectsUseCaseTests: XCTestCase {
+    var repository: MockPetProjectsRepository?
+    
+    var useCase: FetchPetProjectsUseCase?
+    let expectation = XCTestExpectation(description: "UseCase expectation")
+    
+    override func setUpWithError() throws {
+        self.repository = MockPetProjectsRepository()
+        
+        self.useCase = FetchPetProjectsUseCaseImpl(repository: self.repository!)
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testSuccess() async throws {
+        let stringField = PetProjectFieldStringValueFirestoreModel(stringValue: "abc")
+        let fields = PetProjectFieldFirestoreModel(titleKey: stringField, subtitleKey: stringField, iconUrl: stringField, linkAppStore: stringField, linkPlayStore: stringField, linkWeb: stringField, downloads: stringField)
+        let firestoreModel = PetProjectFirestoreModel(name: "abc123", fields: fields)
+        let array = [firestoreModel, firestoreModel]
+        self.repository?.fetchResult = .success(array)
+        
+        let result = await self.useCase?.fetch()
+        switch result {
+        case .success(let petProjects):
+            XCTAssertEqual(petProjects.count, array.count)
+        case .failure(let error):
+            XCTFail("Unexpected error: \(error)")
+        default:
+            XCTFail("Unexpected case")
+        }
+    }
+}
