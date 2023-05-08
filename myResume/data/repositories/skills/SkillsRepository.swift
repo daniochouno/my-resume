8d51313236b3123d5cfe9a622e0923d05e47a8c9
Daniel Martínez <developer@danielmartinez.info>
2023-05-08 20:49:24 +0200

    Added tests for Fetch Skills from repository.

--- myResume/data/repositories/skills/SkillsRepository.swift
+++ myResume/data/repositories/skills/SkillsRepository.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+protocol SkillsRepository {
+    func fetch() async -> Result<[SkillSectionJsonModel], Error>
+}
+
+class SkillsRepositoryImpl: SkillsRepository {
+    let dataSource: JsonDataSource
+    
+    init(dataSource: JsonDataSource) {
+        self.dataSource = dataSource
+    }
+    
+    func fetch() async -> Result<[SkillSectionJsonModel], Error> {
+        return .failure(APIResponseError.configuration)
+    }
+}
--- myResumeTests/data/repositories/skills/SkillsRepositoryFetchTests.swift
+++ myResumeTests/data/repositories/skills/SkillsRepositoryFetchTests.swift
@@ -6,30 +6,37 @@
 //
 
 import XCTest
+@testable import myResume
 
 final class SkillsRepositoryFetchTests: XCTestCase {
-
+    var dataSource: MockJsonDataSource?
+    
+    var repository: SkillsRepository?
+    let expectation = XCTestExpectation(description: "Repository expectation")
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.dataSource = MockJsonDataSource()
+        
+        self.repository = SkillsRepositoryImpl(dataSource: self.dataSource!)
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testSuccess() async throws {
+        let sectionModel = SkillSectionJsonModel(title: "abc", order: 0, items: [])
+        let arraySections = [sectionModel, sectionModel]
+        self.dataSource?.fetchSkillsResult = .success(arraySections)
+        
+        let result = await self.repository?.fetch()
+        switch result {
+        case .success(let works):
+            XCTAssertEqual(works.count, arraySections.count)
+        case .failure(let error):
+            XCTFail("Unexpected error: \(error)")
+        default:
+            XCTFail("Unexpected case")
         }
     }
-
 }
