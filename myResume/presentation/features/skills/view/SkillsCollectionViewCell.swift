b07b31519681f59ea18a3f60578fa1a2e626c276
Daniel Martínez <developer@danielmartinez.info>
2023-05-09 11:14:26 +0200

    Added cells for skill item and header.

---
+++ myResume/presentation/features/skills/view/SkillsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SkillsCollectionViewCell.swift
+//  myResume
+//
+//  Created by Daniel Martínez Muñoz on 9/5/23.
+//
+
+import UIKit
+
+class SkillsCollectionViewCell: UICollectionViewCell {
+    static let identifier = "skillCollectionViewCell"
+    
+    @IBOutlet weak var title: UILabel?
+}
---
+++ myResume/presentation/features/skills/view/SkillsCollectionViewHeader.swift
@@ -0,0 +1,14 @@
+//
+//  SkillsCollectionViewHeader.swift
+//  myResume
+//
+//  Created by Daniel Martínez Muñoz on 9/5/23.
+//
+
+import UIKit
+
+class SkillsCollectionViewHeader: UICollectionReusableView {
+    static let identifier = "skillCollectionViewHeader"
+    
+    @IBOutlet weak var title: UILabel?
+}
