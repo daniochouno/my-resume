6002241e499087e1279f0da8713c96407ac4049a
Daniel Martínez <developer@danielmartinez.info>
2023-05-22 17:53:10 +0200

    Added fetchDetails in Works Repository.

--- myResume/data/repositories/works/WorksRepository.swift
+++ myResume/data/repositories/works/WorksRepository.swift
@@ -9,6 +9,7 @@ import Foundation
 
 protocol WorksRepository {
     func fetch() async -> Result<WorkRepositoryModel, Error>
+    func fetchDetails(id: String) async -> Result<WorkDetailsRepositoryModel, Error>
 }
 
 class WorksRepositoryImpl: WorksRepository {
@@ -71,6 +72,56 @@ class WorksRepositoryImpl: WorksRepository {
         case .success(let documents):
             let works = documents.documents
             return .success(works)
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+    
+    func fetchDetails(id: String) async -> Result<WorkDetailsRepositoryModel, Error> {
+        if let cacheResult = cacheFetchDetails(id: id) {
+            let repositoryModel = WorkDetailsRepositoryModel(type: .localCache, item: cacheResult)
+            return .success(repositoryModel)
+        }
+        
+        let remoteResult = await remoteFetchDetails(id: id)
+        switch remoteResult {
+        case .success(let item):
+            _ = self.cacheDataSource.storeWorkDetails(id: id, item: item)
+            
+            let repositoryModel = WorkDetailsRepositoryModel(type: .remote, item: item)
+            return .success(repositoryModel)
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+    
+    private func cacheFetchDetails(id: String) -> WorkDetailsFirestoreModel? {
+        let cacheResult = cacheDataSource.fetchWorkDetails(id: id)
+        switch cacheResult {
+        case .success(let model):
+            let expirationTimeInSeconds = settingsBundleDataSource.fetchLocalCacheExpirationTimeValue()
+            
+            let now = Date().timeIntervalSince1970
+            let cacheStoredAt = model.createdAt
+            guard ((cacheStoredAt + expirationTimeInSeconds) >= now) else {
+                // Expired cache
+                return nil
+            }
+            
+            let workDetails = model.item
+            return workDetails
+        case .failure:
+            return nil
+        }
+    }
+    
+    private func remoteFetchDetails(id: String) async -> Result<WorkDetailsFirestoreModel, Error> {
+        await authenticateIfNeeded()
+        
+        let result = await remoteDataSource.fetchWorkDetails(id: id)
+        switch result {
+        case .success(let details):
+            return .success(details)
         case .failure(let error):
             return .failure(error)
         }
